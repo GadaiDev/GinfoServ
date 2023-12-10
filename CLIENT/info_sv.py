0bd4235e71f18980b520b5e4d2374d04d010e49a
GadaiDev <152474947+GadaiDev@users.noreply.github.com>
2023-12-10 19:58:21 +0900

    Update info_sv.py

--- CLIENT/info_sv.py
+++ CLIENT/info_sv.py
@@ -1,6 +1,6 @@
 import requests
 
-SV_NAME = "https://info.gadaidev.repl.co/"
+sv_name = ""
 ERROR = """<!doctype html>
 <html lang=en>
 <title>500 Internal Server Error</title>
@@ -8,12 +8,15 @@ ERROR = """<!doctype html>
 <p>The server encountered an internal error and was unable to complete your request. Either the server is overloaded or there is an error in the application.</p>"""
 
 def write(port,text):
-    requests.post(f"{SV_NAME}w",{"port":port,"data":text})
+    requests.post(f"{sv_name}w",{"port":port,"data":text})
     return f""
 def read(port):
-    return requests.post(f"{SV_NAME}r",{"port":port}).text
+    return requests.post(f"{sv_name}r",{"port":port}).text
 def is_exist(port):
     if read(port) == ERROR:
         return False
     else:
         return True
+def setup(url):
+    global sv_name
+    sv_name = url
