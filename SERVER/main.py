0b88e369cbe3a1b2d6f9f0e11bd361b54ddba128
GadaiDev <152474947+GadaiDev@users.noreply.github.com>
2023-12-10 19:50:45 +0900

    Main.py　サーバー側
    最初のコミット

---
+++ SERVER/main.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask, request
+import random
+
+app = Flask(__name__)
+
+
+@app.route('/w', methods=["POST"])
+def write():
+  open(f"port-{request.form.get('port')}.txt", "w",
+       encoding="utf-8").write(request.form.get("data"))
+  return ""
+
+
+@app.route('/r', methods=["POST"])
+def read():
+  txt = open(f"port-{request.form.get('port')}.txt", "r",
+    encoding="utf-8").read()
+  os.remove(f"port-{request.form.get('port')}.txt")
+  return txt
+
+app.run(host='0.0.0.0', port=8000)
