c5f9e346cfdc135427d628752c11beb10972ea39
GadaiDev <152474947+GadaiDev@users.noreply.github.com>
2023-12-14 23:02:01 +0900

    Update main.py

--- SERVER/main.py
+++ SERVER/main.py
@@ -15,8 +15,18 @@ def write():
 @app.route('/r', methods=["POST"])
 def read():
   txt = open(f"port-{request.form.get('port')}.txt", "r",
-    encoding="utf-8").read()
+             encoding="utf-8").read()
   os.remove(f"port-{request.form.get('port')}.txt")
   return txt
+
+
+@app.route('/e', methods=["POST"])
+def exist():
+  try:
+    txt = open(f"port-{request.form.get('port')}.txt", "r", encoding="utf-8")
+    return "true"
+  except FileNotFoundError:
+    return "false"
+
 
 app.run(host='0.0.0.0', port=8000)
